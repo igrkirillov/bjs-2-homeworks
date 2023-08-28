deeec5f91bb9d2ec498b2372d1e01bfa343a31a9
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-08-28 22:26:09 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  const d = Math.pow(b,2) - 4*a*c;
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    return [-b/(2*a)];
+  } else {
+    return [(-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a)];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const credit = amount - contribution;
+  const monthPercent = percent / 100 / 12;
+  const paymentOfMonth = credit * (monthPercent + (monthPercent / (((1 + monthPercent)**countMonths) - 1)));
+  return +(paymentOfMonth * countMonths).toFixed(2);
 }
