77933858775075315a9b70a125c5c4fa71c7af25
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-09-10 00:37:22 +0300

    Задание со студентом (инкапсулирование логики). Правда непонятно, почему мы функции добавляем в прототип, а не напрямую в Student

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty("marks")) {
+      this.marks = [...this.marks, ...marks];
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty("marks") && this.marks.length > 0) {
+        return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  this.excluded = reason;
 }
