2a9c36f584a258826928c92bf7eb27c495bbc3b5
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-09-10 20:40:20 +0300

    triangles

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,51 @@
-﻿
+﻿function parseCount(input) {
+    const count = Number.parseFloat(input);
+    if (isNaN(count)) {
+        throw new Error("Невалидное значение");
+    }
+    return count;
+}
+
+function validateCount(input) {
+    try {
+        return parseCount(input);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        const m = "Ошибка! Треугольник не существует";
+        return {
+            get perimeter() {
+                return m;
+            },
+            get area() {
+                return m;
+            }
+        }
+    }
+}
